a4ac6d4892ff4af21b5929d91a37cccd44ce276f
bebni <abhishekbebni11@gmail.com>
2024-03-13 12:41:21 +0530

    comitted changes

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, make_response, send_file
-from OpenSSL import SSL
+#from OpenSSL import SSL
 
 
 # Define SSL certificate and key file paths 
@@ -8,8 +8,8 @@ KEY_FILE = "/root/hostnamescript/pyhostname/certificate/cx-server/key.pem"
 
 # Create SSL context
 #import ssl context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
-context = SSL.Context(SSL.PROTOCOL_TLSv1_2)
-context.load_cert_chain(CERT_FILE, KEY_FILE)
+#context = SSL.Context(SSL.PROTOCOL_TLSv1_2)
+#context.load_cert_chain(CERT_FILE, KEY_FILE)
 
 app = Flask(__name__)
 
